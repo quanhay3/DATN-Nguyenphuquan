4fce09cb4f87dbf668aea6b50d26171d90f2f72a
quanhay3 <quanhay23@gmail.com>
2024-11-07 00:13:17 +0700

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
