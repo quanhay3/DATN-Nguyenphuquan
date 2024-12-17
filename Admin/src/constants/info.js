76906d2574ff7730b2419fbe880e185543e0ce99
quanhay3 <quanhay23@gmail.com>
2024-12-17 11:49:31 +0700

    fix footer

--- Admin/src/constants/info.js
+++ Admin/src/constants/info.js
@@ -3,7 +3,7 @@ export const info = {
     email: "nuocmamphunghia@gmail.com",
     phoneNumber: "1900 8866 18",
     websiteUrl: 'https://nuocmamphunghia.com/',
-    websiteName: "nuocmamphunghia.com",
+    websiteName: "datn.vti-lab.com",
     mapLink: "https://maps.app.goo.gl/CmkQxYL5LWPr4Zp18",
     facebookLink: "https://web.facebook.com/profile.php?id=61555308231048&_rdc=1&_rdr",
     zaloLink: "https://chat.zalo.me/?phone=0862174817",
