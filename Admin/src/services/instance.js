2d4c6849deed2ffe5f82de5dc79fb69e5350eb76
quanhay3 <quanhay23@gmail.com>
2025-01-05 00:48:10 +0700

    fix service instance fe

--- Admin/src/services/instance.js
+++ Admin/src/services/instance.js
@@ -8,6 +8,7 @@ const instance = axios.create({
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
     "Access-Control-Allow-Headers": "Content-Type",
+    "X-Custom-Header": "random-value-1234567890",
   },
 });
 
